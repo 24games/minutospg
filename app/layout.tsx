bba68304399497627175cfdae8c87a70ad5f6b96
24games <dev@24games.com>
2026-01-18 17:18:01 -0300

    Adicionar Google Tag Manager no layout do site

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,31 @@ export default function RootLayout({
   return (
     <html lang="pt-BR" className="bg-black">
       <body className="antialiased bg-black">
+        {/* Google Tag Manager */}
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-KW6XRGDG');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KW6XRGDG"
+            height="0" 
+            width="0" 
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         {/* Meta Pixel Code */}
         <Script
           id="meta-pixel"
